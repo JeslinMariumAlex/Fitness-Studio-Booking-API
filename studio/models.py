d32c89e0b486fa93799f359176ee012c8dd309dd
Jeslin <jeslinmariumalex@gmail.com>
2025-06-11 14:18:51 +0530

    Created models FitnessClass and Booking

--- studio/models.py
+++ studio/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class FitnessClass(models.Model):
+    name = models.CharField(max_length=100)
+    instructor = models.CharField(max_length=100)
+    datetime = models.DateTimeField()
+    total_slots = models.IntegerField()
+    available_slots = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name} by {self.instfructor} on {self.datetime}"
+    
+class Booking(models.Model):
+    fitness_class = models.ForeignKey(FitnessClass, on_delete=models.CASCADE)
+    client_name = models.CharField(max_length=100)
+    client_email = models.EmailField()
+    booked_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.client_name} - {self.fitness_class.name}"
